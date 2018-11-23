3a77f55739998ad8f324bc25cb15daca2055e9ac
Guyeol Jeong <jigu8534@gmail.com>
2018-11-23 20:01:19 +0900

    feat: complete google search function

--- src/query/GoogleQuery.js
+++ src/query/GoogleQuery.js
@@ -2,59 +2,27 @@
  * @author guyeol, jeong
  * Copyright © 2018 guyeol_jeong. All rights reserved.
  */
+
 var GoogleQuery = function (obj) {
 	var gQuery = "";
-
 	for (var ob in obj) {
-		// 정의되었지만 비어있거나 정의되지 않았을 때,
 		if (typeof ob === "undefined" || obj[ob].length === 0) {
 			console.log("NO");
 		}
-		// 데이터 타입이 string일 때,
 		else if (typeof obj[ob] === "string") {
 			switch (ob) {
 				case 'basic': gQuery += obj[ob] + ' '; break;
-				case 'site': site(obj[ob]); break;
-				case 'type': filetype(obj[ob]); break;
+				case 'site': gQuery += "site:" + obj[ob] + ' '; break;
+				case 'type': gQuery += "filetype:" + obj[ob] + ' '; break;
 			}
-		}
-		// 데이터 타입이 array일 때
 		else {
 			switch (ob) {
-				case 'exact': exact(obj[ob]); break;
+				case 'exact': for (var a of obj[ob]) { gQuery += "\"" + a + "\""; } break;
 				case 'or': break;
-				case 'exclude': exclude(obj[ob]); break;
-				case 'type': break;
+				case 'exclude': for (var a of obj[ob]) { gQuery += '-' + a + ' '; } break;
 			}
 		}
 	}
-
-	function exact (o) {
-		for (var a of o) {
-			gQuery += '\"' + a + '\"';
-			gQuery += ' ';
-		}
-	}
-
-	function or (o) {
-
-	}
-
-	function exclude (o) {
-		for (var a of o) {
-			gQuery += '-' + a;
-			gQuery += ' ';
-		}
-	}
-
-	function site (o) {
-		gQuery += "site:" + o;
-	}
-
-	function filetype (o) {
-		gQuery += "filetype:" + o;
-	}
-
 	return gQuery;
 };
 
