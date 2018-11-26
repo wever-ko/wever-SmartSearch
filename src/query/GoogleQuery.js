07be093322ed8fed960e47646fd9b8c99f5e370f
Guyeol Jeong <jigu8534@gmail.com>
2018-11-26 15:27:29 +0900

    fix: Invalid parenthesis correction

--- src/query/GoogleQuery.js
+++ src/query/GoogleQuery.js
@@ -11,15 +11,16 @@ var GoogleQuery = function (obj) {
 		}
 		else if (typeof obj[ob] === "string") {
 			switch (ob) {
-				case 'basic': gQuery += obj[ob] + ' '; break;
-				case 'site': gQuery += "site:" + obj[ob] + ' '; break;
-				case 'file': gQuery += "filetype:" + obj[ob] + ' '; break;
+				case 'basic': gQuery += obj[ob]; break;
+				case 'site': gQuery += " site:" + obj[ob] + ' '; break;
+				case 'file': gQuery += " filetype:" + obj[ob] + ' '; break;
 			}
+		}
 		else {
 			switch (ob) {
-				case 'exact': for (var a of obj[ob]) { gQuery += "\"" + a + "\""; } break;
+				case 'exact': for (var a of obj[ob]) { gQuery += " \"" + a + "\""; } break;
 				case 'or': break;
-				case 'exclude': for (var a of obj[ob]) { gQuery += '-' + a + ' '; } break;
+				case 'exclude': for (var a of obj[ob]) { gQuery += ' -' + a + ' '; } break;
 			}
 		}
 	}
