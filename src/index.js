2c89585df3a11366496ae0b4662c50cfee4e7e16
Guyeol Jeong <jigu8534@gmail.com>
2018-12-11 05:44:49 +0900

    feat: import newtab module

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import {GoogleQuery} from './query/GoogleQuery';
 import {TwitterQuery} from './query/TwitterQuery';
 import {YouTubeQuery} from './query/YouTubeQuery';
 import {NaverQuery} from './query/NaverQuery';
+import {NewTab} from './utils/NewTab';
 
 export {GoogleQuery, TwitterQuery, YouTubeQuery, NaverQuery};
