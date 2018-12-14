7ecf44084e881c70ecb25e49c67f8aaba0bb5608
Heejun Lee <zaqq1414@gmail.com>
2018-12-14 12:23:42 +0900

    add History to index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import {GoogleQuery} from './query/GoogleQuery';
 import {TwitterQuery} from './query/TwitterQuery';
 import {YouTubeQuery} from './query/YouTubeQuery';
 import {NaverQuery} from './query/NaverQuery';
+import {History} from './cookie/history';
 
-export {GoogleQuery, TwitterQuery, YouTubeQuery, NaverQuery};
+export {GoogleQuery, TwitterQuery, YouTubeQuery, NaverQuery, History};
